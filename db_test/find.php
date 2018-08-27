3da2c5578edbaf50804ac3470f18f07fdab39bab
Shinjia <shinjia168@gmail.com>
2018-08-27 21:56:22 +0800

    class10

---
+++ db_test/find.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Title of the document</title>
+</head>
+
+<body>
+<h1>Hello World Hello</h1>
+<form method="post" action="find_x.php">
+姓名：<input type="text" name="key" >
+<br />
+<input type="submit" value="查詢">
+</form>
+</body>
+</html>
